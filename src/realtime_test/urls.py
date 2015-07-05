81a932c9a6fe96f7aec1eee0b25d81bbc0d222b2
sztankatt <sztankatt@gmail.com>
2015-07-05 18:32:36 +0000

    removing realtime test

--- src/realtime_test/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-	url(r'^$', 'realtime_test.views.home', name='home'),
-	url(r'^alert/$', 'realtime_test.views.alert', name='alert'),
-)
