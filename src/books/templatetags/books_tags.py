039a66d882119e8161d2551f5cb12954af7d5226
Tamas Sztanka-Toth <sztankatt@gmail.com>
2015-09-11 03:43:06 +0100

    Added display of rating, with stars

---
+++ src/books/templatetags/books_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from django.template.loader import get_template
+from django.template import Context
+
+register = template.Library()
+
+
+@register.filter(name='star_rating')
+def star_rating(rating):
+    template = get_template('after_login/books/templatetags/star_rating.html')
+    number = int(rating)
+
+    half = 0.0
+
+    if (rating - number) >= 0.5:
+        half = 0.5
+
+    remaining = 4-number
+
+    stars = number*[1.0] + [half] + remaining*[0.0]
+
+    c = Context({'stars':stars})
+
+    content = template.render(c)
+
+    return content
